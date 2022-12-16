6bfcc3b1237138706a2962b56001de7d4c7eb534
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-12-17 00:37:52 +0100

    M

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
-"""
-import sys
-import urllib.request
+"""post email api call"""
 
-if __name__ == "__main__":
-    url = sys.argv[1]
+if __name__ == '__main__':
+    import requests
+    import sys
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    params = {'email': sys.argv[2]}
+    res = requests.post(sys.argv[1], data=params)
+    print(res.text)
